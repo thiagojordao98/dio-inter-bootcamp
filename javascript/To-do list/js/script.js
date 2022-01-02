f9f7f53f807d98b1c61d35813a165ab3f1bf07a6
Thiago Jordão <thiagojordao98@gmail.com>
2022-01-01 23:48:32 -0300

    add scrip.js

--- javascript/To-do list/js/script.js
+++ javascript/To-do list/js/script.js
@@ -1,3 +1,21 @@
-function addEvent(){
-  document.getElementById('check').innerHTML = " ";
+var itemLista = document.getElementById("ul");
+function addTarefa() {
+    if (document.getElementById("itemLista").value !== "") {
+        var tarefa = document.createElement("li");
+        tarefa.innerHTML = document.getElementById("itemLista").value;
+        
+        var remover = document.createElement("a");
+        remover.innerHTML = "Remover";
+        remover.setAttribute("href", "javascript:;"); // apenas para parecer um link
+        remover.onclick = function() {
+            // remove apenas "tarefa", que é o li
+            itemLista.removeChild(tarefa);
+        }
+
+        // adiciona o link de remover ao li
+        tarefa.appendChild(remover);
+        // adiciona o li ao ul
+        itemLista.appendChild(tarefa);
+        document.getElementById("itemLista").value = "";
+    }
 }
